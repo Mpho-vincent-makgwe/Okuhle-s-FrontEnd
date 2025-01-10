ace6636a9758ea50eac6e6c8bf208e812b47233a
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-01-10 13:20:44 +0200

    theming

--- static/js/script.js
+++ static/js/script.js
@@ -49,15 +49,5 @@ document.addEventListener('DOMContentLoaded', () => {
     // Periodically update data
     setInterval(fetchAndUpdateSensorData, 5000); // Every 5 seconds
 
-    // Theme toggle functionality
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        body.classList.add('dark-mode');
-    }
 
-    themeToggleButton.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-        const isDarkMode = body.classList.contains('dark-mode');
-        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-    });
 });
